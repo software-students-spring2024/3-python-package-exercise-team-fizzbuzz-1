9201ccb78b4222dd012a908884726359662ae95f
DoodyShark <da2863@nyu.edu>
2024-03-21 13:29:53 -0700

    Fixed github actions

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,6 +1,10 @@
-from virtual_pet.pet import Pet, create_pet
+"""Testing the functionalities of virtual pet module"""
+
+from src.virtual_pet.pet import Pet
+from src.virtual_pet.utilities import create_pet
 
 def test_create_pet():
+    """Tests that creation works"""
     pet = create_pet("Fido", "dog")
     assert pet.name == "Fido"
     assert pet.species == "dog"
@@ -10,32 +14,40 @@ def test_create_pet():
     assert pet.thirst == 0
 
 def test_create_empty_name():
+    """Tests creating an empty named pet"""
     pet = create_pet("", "dog")
     assert pet is None
 
 def test_create_empty_species():
+    """Tests creating an empty with no species"""
     pet = create_pet("Fido", "")
+    assert pet is None
+    pet = create_pet("Fido")
     assert pet is None
 
 def test_feed():
+    """Tests feeding the pet"""
     pet = Pet("Hallie", "cat")
     pet.hunger = 5
     pet.feed(1)
     assert pet.hunger == 4
 
 def test_feed_not_hungry():
+    """Tests feeding the pet if the pet isn't hungry"""
     pet = Pet("Hallie", "cat")
     pet.hunger = 0
     pet.feed(1)
     assert pet.hunger == 0
 
 def test_feed_multiple():
+    """Tests feeding multiple feed points"""
     pet = Pet("Hallie", "cat")
     pet.hunger = 5
     pet.feed(3)
     assert pet.hunger == 2
 
 def test_feed_multiple_times():
+    """Tests feeding mutliple times"""
     pet = Pet("Hallie", "cat")
     pet.hunger = 5
     pet.feed(2)
@@ -44,6 +56,7 @@ def test_feed_multiple_times():
     assert pet.hunger == 1
 
 def test_exercise():
+    """Tests exercising under normal conditions"""
     pet = Pet("Hallie", "cat")
     pet.boredom = 5
     pet.happiness = 5
@@ -52,7 +65,9 @@ def test_exercise():
     assert pet.boredom == 4
     assert pet.happiness == 6
     assert pet.hunger == 6
+
 def test_exercise_max_hunger():
+    """Tests exercising when at max hunger"""
     pet = Pet("Hallie", "cat")
     pet.boredom = 5
     pet.happiness = 5
@@ -61,7 +76,9 @@ def test_exercise_max_hunger():
     assert pet.boredom == 5
     assert pet.happiness == 5
     assert pet.hunger == 10
+
 def test_exercise_long_duration():
+    """Tests exercising for a long duration"""
     pet = Pet("Hallie", "cat")
     pet.boredom = 5
     pet.happiness = 5
@@ -70,4 +87,3 @@ def test_exercise_long_duration():
     assert pet.boredom == 0
     assert pet.happiness == 10
     assert pet.hunger == 10
-
