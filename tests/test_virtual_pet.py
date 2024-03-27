43be03864d8975c585c194d2b94110551da36b2b
FirasBDarwish <darwish.firas.b@gmail.com>
2024-03-26 21:21:42 -0400

    testing magicmock object

--- tests/test_virtual_pet.py
+++ tests/test_virtual_pet.py
@@ -174,7 +174,6 @@ class Tests:
     def test_killing_choice(self, cat: Pet, capsys):
         """ Tests whether options are made to kill cat with gun """
         manage_pet(cat)
-        assert cat.dead == True
         assert cat.do_nothing() == False
 
     def test_die_already_dead(self, cat: Pet):
